558a0128cf7d833a479b47616b1e7030c6a2bdda
dheijligers <dheijligers@quintor.local>
2019-12-18 14:38:44 +0100

    fixed websocket to render incoming messages instantly after reception

--- bmo/src/app/app.component.ts
+++ bmo/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { WebSocketAPI } from './utils/web.socket.api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'bmo';
+
+  webSocketAPI: WebSocketAPI;
+  greeting: string;
+  name: string;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.webSocketAPI = new WebSocketAPI(this);
+    this.webSocketAPI._connect();
+  }
+
+  ngOnDestroy() {
+    this.webSocketAPI._disconnect();
+  }
+
+  sendMessage() {
+    this.webSocketAPI._send(this.name);
+  }
+
+  handleMessage(message) {
+    this.greeting = message;
+  }
 }
